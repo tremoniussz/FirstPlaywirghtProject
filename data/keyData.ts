ee4f6c1a1a614a50c380e6c0b74facd46f50dd60
tremoniussz <99811528+tremoniussz@users.noreply.github.com>
2023-03-28 10:18:22 +0200

    Add files via upload

---
+++ data/keyData.ts
@@ -0,0 +1,3 @@
+export enum KeysDictionary {
+  ENTER = 'Enter'
+}
---
+++ data/taskName.ts
@@ -0,0 +1,4 @@
+export enum DictionaryTaskNames {
+  ToDoName = "Task name to add on the list",
+  ToDeleteName = "Task name to add add delete"
+}
