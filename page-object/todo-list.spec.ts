e3600f9a508df41a32fe6abb78273c19b9092d02
tremoniussz <99811528+tremoniussz@users.noreply.github.com>
2023-03-28 10:59:41 +0200

    Add files via upload

---
+++ page-object/todo-list.spec.ts
@@ -0,0 +1,56 @@
+import { test, expect, chromium, Browser, Page } from "@playwright/test";
+import { TodoListPage } from "../page-object/toDoListPage";
+import { DictionaryTaskNames } from "../data/taskName";
+
+// Define the test suite for the Todo List page
+test.describe("Todo List", () => {
+  let browser: Browser;
+  let page: Page;
+  let todoListPage: TodoListPage;
+
+
+  // Launch the browser and create a new page before running the test suite
+  test.beforeAll(async () => {
+    browser = await chromium.launch();
+    page = await browser.newPage();
+    todoListPage = new TodoListPage(page);
+  });
+
+  // Close the browser after running the test suite
+  test.afterAll(async () => {
+    await browser.close();
+  });
+
+  // Navigate to the Todo List page before each test
+  test.beforeEach(async () => {
+    await todoListPage.navigate();
+  });
+
+  //  Test adding a new item to the Todo List
+  test("should add a new item to todo list", async () => {
+    await todoListPage.inputItemName(DictionaryTaskNames.ToDoName)
+    await todoListPage.pressEnterToAddNewItem()
+    await todoListPage.refreshItemList();
+    expect((await todoListPage.getAllTextContextFromLiItems()).includes(DictionaryTaskNames.ToDoName));
+  });
+
+  //Test deleting an item from the Todo List
+  test("should add and remove item from todo list", async () => {
+    await todoListPage.inputItemName(DictionaryTaskNames.ToDeleteName)
+    await todoListPage.pressEnterToAddNewItem()
+    await todoListPage.refreshItemList();
+    await todoListPage.deleteItemFromListByItemName(DictionaryTaskNames.ToDeleteName);
+    expect((await todoListPage.getAllTextContextFromLiItems()).includes(DictionaryTaskNames.ToDeleteName)).not;
+  });
+
+  //Test to check count elemenst on the list
+  test("should incresse item list size by one after adding new element", async () => {
+    const listSizeBeforeAdding = await todoListPage.getToDoItemListSize();
+    await todoListPage.inputItemName(DictionaryTaskNames.ToDoName)
+    await todoListPage.pressEnterToAddNewItem()
+    await todoListPage.refreshItemList();
+    const listSizeAfterAddNewItem = await todoListPage.getToDoItemListSize();
+    expect(listSizeAfterAddNewItem).toBe(listSizeBeforeAdding + 1);
+  });
+
+});
